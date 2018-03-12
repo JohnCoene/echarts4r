193a8daf49627c33d33ffea792cad60c6479473c
John Coene <jcoenep@gmail.com>
2018-03-12 23:55:56 +0100

    data build FOos

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,38 @@
     unlist() %>%
     unname()
 }
+
+.build_xy <- function(data, x, serie, size){
+  
+  if(!missing(size)){
+    data %>%
+      dplyr::select(!!serie, !!size) -> rhs
+  } else {
+    data %>%
+      dplyr::select(!!serie) -> rhs
+  }
+  
+  lhs <- data.frame(
+    x = x
+  )
+  
+  df <- cbind.data.frame(lhs, rhs)
+  
+  matrix <- unname(as.matrix(df))
+  
+  apply(matrix, 1, as.list)
+}
+
+.build_candle <- function(data, opening, closing, low, high){
+  data %>%
+    dplyr::select(
+      !!opening, 
+      !!closing, 
+      !!low, 
+      !!high
+    ) -> data
+  
+  matrix <- unname(as.matrix(data))
+  
+  apply(matrix, 1, as.list)
+}
