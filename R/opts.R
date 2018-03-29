bae0c931ddba5c4fd5855d53ddcf2a31d6c916c5
Jean-Philippe Coene <jean-philippe.coene@weforum.org>
2018-03-29 11:26:06 +0200

    visual map example added

--- R/opts.R
+++ R/opts.R
@@ -4,6 +4,39 @@
 #' @param calculable Whether show handles, which can be dragged to adjust "selected range".
 #' @param type One of \code{continuous} or \code{piecewise}.
 #' 
+#' @examples 
+#' v <- LETTERS[1:10]
+#' matrix <- data.frame(
+#'   x = sample(v, 300, replace = TRUE), 
+#'   y = sample(v, 300, replace = TRUE), 
+#'   z = rnorm(300, 10, 1),
+#'   color = rnorm(300, 10, 1),
+#'   size = rnorm(300, 10, 1),
+#'   stringsAsFactors = FALSE
+#' ) %>% 
+#'   dplyr::group_by(x, y) %>% 
+#'   dplyr::summarise(
+#'     z = sum(z),
+#'     color = sum(color),
+#'     size = sum(size)
+#'   ) %>% 
+#'   dplyr::ungroup() 
+#'   
+#' matrix %>% 
+#'   e_charts(x) %>% 
+#'   e_scatter_3d(y, z, color, size) %>% 
+#'   e_visual_map(
+#'     min = 1, max = 100,
+#'     inRange = list(symbolSize = c(1, 30)), # scale size
+#'     dimension = 3 # third dimension 0 = x, y = 1, z = 2, size = 3
+#'   ) %>% 
+#'   e_visual_map(
+#'     min = 1, max = 100,
+#'     inRange = list(color = c('#bf444c', '#d88273', '#f6efa6')), # scale colors
+#'     dimension = 4, # third dimension 0 = x, y = 1, z = 2, size = 3, color = 4
+#'     bottom = 300 # padding to avoid visual maps overlap
+#'   )
+#' 
 #' @export
 e_visual_map <- function(e, calculable = TRUE, type = c("continuous", "piecewise"), ...){
   
